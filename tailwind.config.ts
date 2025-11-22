07315fcb3ed189ab820f03646641448b357580a0
Kaizan <jplayz255@gmail.com>
2025-11-22 14:21:25 +0000

    still is there

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,25 +80,10 @@ export default {
           '0%, 100%': { opacity: '0.5' },
           '50%': { opacity: '1' },
         },
-        'blob-move': {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(30px, -50px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-20px, 20px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'blob-move': 'blob-move 20s ease-in-out infinite',
       },
     },
   },
