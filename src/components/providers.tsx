fc5656ce6a3531fe6b212119bf206acd40c8fa80
Kaizan <jplayz255@gmail.com>
2025-11-22 22:35:18 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,16 +6,7 @@ import { ThemeProvider } from "@/context/theme-provider";
 import { VideoProvider } from "@/context/video-provider";
 import { LoadingProvider } from "@/context/loading-provider";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
-import type { ToastFn, ToasterToast } from "@/hooks/use-toast";
 import { WatchlistProvider } from "@/context/watchlist-provider";
-
-interface ToastContextType {
-  toasts: ToasterToast[];
-  toast: ToastFn;
-  dismiss: (toastId?: string) => void;
-}
-
-export const ToastContext = React.createContext<ToastContextType | undefined>(undefined);
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
