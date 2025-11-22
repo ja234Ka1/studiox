10fd67e6d81d27ee1f0c0e9c6b30538ebd8fb709
Kaizan <jplayz255@gmail.com>
2025-11-22 12:31:28 +0000

    make other cards around the one hovered over move aswell to make it fit

--- src/components/media-carousel.tsx
+++ src/components/media-carousel.tsx
@@ -26,16 +26,20 @@ export default function MediaCarousel({ title, items }: MediaCarouselProps) {
         opts={{
           align: "start",
           dragFree: true,
+          // Set a larger container for slides to overflow
+          containScroll: 'keepSnaps',
         }}
         className="w-full"
       >
-        <CarouselContent className="-ml-4 py-12">
+        <CarouselContent className="-ml-4 py-12 px-2">
           {items.map((item, index) => (
             <CarouselItem
               key={`${item.id}-${index}`}
               className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 pl-4"
             >
-              <MediaCard item={item} />
+              <div className="p-1">
+                 <MediaCard item={item} />
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
