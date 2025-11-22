121144425ef034eb15037e9e2b713f9c474c1ea8
Kaizan <jplayz255@gmail.com>
2025-11-22 13:26:38 +0000

    Try fixing this error: `Runtime Error: Error: Failed to execute 'animate

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -45,7 +45,7 @@ export default function LoadingScreen() {
       opacity: 1,
       transition: {
         duration: 0.5,
-        ease: [0.6, 0.01, -0.05, 0.95],
+        ease: [0.6, 0.01, 0.05, 0.95],
       },
     },
   };
