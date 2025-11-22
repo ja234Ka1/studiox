94de23f83c22079fe7b18725b0b08974ab6408f9
Kaizan <jplayz255@gmail.com>
2025-11-22 22:28:40 +0000

    Try fixing this error: `Runtime Error: Error: Maximum update depth excee

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -8,45 +8,44 @@ import { LoadingProvider } from "@/context/loading-provider";
 import { ToastContext, type ToasterToast } from "@/hooks/use-toast";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 
+const TOAST_LIMIT = 3;
 let count = 0;
+
 function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER;
   return count.toString();
 }
 
-const TOAST_LIMIT = 3;
-
 export function ToastProvider({ children }: { children: React.ReactNode }) {
   const [toasts, setToasts] = React.useState<ToasterToast[]>([]);
 
-  const toast = React.useCallback((props: Omit<ToasterToast, "id">) => {
-    const id = genId();
-    
-    const update = (props: Partial<ToasterToast>) => {
-        setToasts((prev) =>
+  const toast = React.useCallback(
+    (props: Omit<ToasterToast, "id">) => {
+      const id = genId();
+      const newToast: ToasterToast = {
+        ...props,
+        id,
+        open: true,
+        onOpenChange: (open) => {
+          if (!open) {
+            setToasts((prev) => prev.filter((t) => t.id !== id));
+          }
+        },
+      };
+
+      setToasts((prev) => [newToast, ...prev].slice(0, TOAST_LIMIT));
+
+      return {
+        id: id,
+        dismiss: () => setToasts((prev) => prev.filter((t) => t.id !== id)),
+        update: (props: Partial<ToasterToast>) =>
+          setToasts((prev) =>
             prev.map((t) => (t.id === id ? { ...t, ...props } : t))
-        );
-    };
-
-    const dismiss = () => {
-        setToasts((prev) => prev.filter((t) => t.id !== id));
-    };
-
-    const newToast: ToasterToast = {
-      ...props,
-      id,
-      open: true,
-      onOpenChange: (open) => {
-        if (!open) {
-          dismiss();
-        }
-      },
-    };
-
-    setToasts((prev) => [newToast, ...prev].slice(0, TOAST_LIMIT));
-
-    return { id, dismiss, update };
-  }, [toasts]);
+          ),
+      };
+    },
+    [] // No dependencies, as we use the functional update form of setState
+  );
 
   const dismiss = React.useCallback((toastId?: string) => {
     setToasts((prev) =>
