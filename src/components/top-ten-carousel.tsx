bd9624151e4e9a67f133ff5085e3250c242ae4f7
Kaizan <jplayz255@gmail.com>
2025-11-23 20:44:05 +0000

    it doesnt show the 10th card make sure it  does

--- src/components/top-ten-carousel.tsx
+++ src/components/top-ten-carousel.tsx
@@ -61,12 +61,13 @@ export default function TopTenCarousel() {
         plugins={[plugin.current]}
         className="w-full"
       >
-        <CarouselContent className="-ml-2">
+        <CarouselContent>
           {mediaItems.map((item, index) => (
             <CarouselItem
               key={item.id}
               className={cn(
-                "basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6 pl-24 group"
+                "basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6 group",
+                "pl-24"
               )}
             >
                 <div className="flex items-center transition-transform duration-300 ease-in-out group-hover:scale-105">
@@ -76,7 +77,7 @@ export default function TopTenCarousel() {
                     >
                         {index + 1}
                     </span>
-                    <div className="w-full shrink-0 transition-transform duration-300 ease-in-out group-hover:scale-110 -ml-8">
+                    <div className="w-full shrink-0 transition-transform duration-300 ease-in-out group-hover:scale-110">
                         <MediaCard item={item} />
                     </div>
                 </div>
