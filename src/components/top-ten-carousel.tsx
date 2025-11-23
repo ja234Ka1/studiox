315adee07c456f80dfbf002ece2d619f7030a663
Kaizan <jplayz255@gmail.com>
2025-11-23 20:43:08 +0000

    make sure the carisole doesnt do a full carisol and stops at 10

--- src/components/top-ten-carousel.tsx
+++ src/components/top-ten-carousel.tsx
@@ -56,7 +56,7 @@ export default function TopTenCarousel() {
       <Carousel
         opts={{
           align: "start",
-          loop: true,
+          loop: false,
         }}
         plugins={[plugin.current]}
         className="w-full"
@@ -66,7 +66,7 @@ export default function TopTenCarousel() {
             <CarouselItem
               key={item.id}
               className={cn(
-                "basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6 pl-24 pr-2 group"
+                "basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6 pl-24 group"
               )}
             >
                 <div className="flex items-center transition-transform duration-300 ease-in-out group-hover:scale-105">
