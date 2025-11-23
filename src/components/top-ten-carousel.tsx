c0674e6516a72db7668a6d881a65e5d86090dad8
Kaizan <jplayz255@gmail.com>
2025-11-23 20:53:35 +0000

    I WANT THE CARDS HAVE MORE SPACING BETWEEN EACH OTHER NOT MORE SPACING B

--- src/components/top-ten-carousel.tsx
+++ src/components/top-ten-carousel.tsx
@@ -66,15 +66,15 @@ export default function TopTenCarousel() {
             <CarouselItem
               key={item.id}
               className={cn(
-                "group pl-4 sm:pl-8 md:pl-12 lg:pl-16",
-                "basis-1/3 sm:basis-1/4 md:basis-1/5 lg:basis-1/6"
+                "group pl-8 sm:pl-12 md:pl-16 lg:pl-20",
+                "basis-2/5 sm:basis-1/3 md:basis-1/4 lg:basis-1/5"
               )}
             >
                 <div className="flex items-center transition-transform duration-300 ease-in-out group-hover:scale-105">
                     <span 
                         style={{ WebkitTextStroke: '3px hsl(var(--primary))' }}
                         className={cn(
-                            "text-[200px] font-black text-transparent transition-all duration-300 ease-in-out group-hover:text-primary mr-2"
+                            "text-[120px] font-black text-transparent transition-all duration-300 ease-in-out group-hover:text-primary mr-2"
                         )}
                     >
                         {index + 1}
