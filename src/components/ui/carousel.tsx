b8e4ad091fc7924ea5e143a583c13a8677a19f0b
Kaizan <jplayz255@gmail.com>
2025-11-23 20:55:37 +0000

    still modifiying the wrong item i want the CARDS not the numbers

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -163,7 +163,7 @@ const CarouselContent = React.forwardRef<
         ref={ref}
         className={cn(
           "flex",
-          orientation === "horizontal" ? "px-4 md:px-8" : "-mt-4 flex-col px-0",
+          orientation === "horizontal" ? "px-8 md:px-16" : "-mt-4 flex-col px-0",
           className
         )}
         {...props}
