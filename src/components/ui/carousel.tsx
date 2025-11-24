d231077eda7bed194b89caff2d8c61e925b9b719
Kaizan <jplayz255@gmail.com>
2025-11-24 18:00:23 +0000

    preevent clipping of the cards and make sure it can exit and expand out

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -158,7 +158,7 @@ const CarouselContent = React.forwardRef<
   const { carouselRef, orientation } = useCarousel()
 
   return (
-    <div ref={carouselRef} className="overflow-hidden">
+    <div ref={carouselRef} className="py-4">
       <div
         ref={ref}
         className={cn(
